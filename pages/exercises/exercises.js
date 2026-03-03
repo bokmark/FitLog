c271f66b7eb8cbeed6e32f97964fe34d4e7c46d1
mashuqian <mashuqian@didiglobal.com>
2026-03-03 11:45:21 +0800

    feat: 今日训练
    Signed-off-by: mashuqian <mashuqian@didiglobal.com>

--- pages/exercises/exercises.js
+++ pages/exercises/exercises.js
@@ -1,4 +1,5 @@
 const planService = require('../../services/plan-service')
+const dateUtil = require('../../utils/date')
 
 Page({
   data: {
@@ -7,7 +8,9 @@ Page({
     keyword: '',
     totalCount: 0,
     showMedia: false,
-    currentMedia: null
+    currentMedia: null,
+    showDayPicker: false,
+    dayOptions: []
   },
 
   onLoad() {
@@ -62,5 +65,38 @@ Page({
 
   onCloseMedia() {
     this.setData({ showMedia: false, currentMedia: null })
+  },
+
+  onAddTap() {
+    const plan = planService.getActivePlan()
+    if (!plan) {
+      wx.showToast({ title: '请先创建训练计划', icon: 'none' })
+      return
+    }
+    const dayNames = dateUtil.DAY_NAMES
+    const dayOptions = []
+    for (let d = 0; d < 7; d++) {
+      const day = plan.days[d] || plan.days[String(d)]
+      if (day && !day.isRestDay) {
+        dayOptions.push({ dayIndex: d, label: dayNames[d] + (day.name ? '（' + day.name + '）' : '') })
+      }
+    }
+    if (dayOptions.length === 0) {
+      wx.showToast({ title: '所有天都是休息日', icon: 'none' })
+      return
+    }
+    this.setData({ showDayPicker: true, dayOptions })
+  },
+
+  onPickDay(e) {
+    const dayIndex = e.currentTarget.dataset.day
+    this.setData({ showDayPicker: false })
+    wx.navigateTo({
+      url: `/pages/exercise-edit/exercise-edit?dayIndex=${dayIndex}&mode=add`
+    })
+  },
+
+  onClosePicker() {
+    this.setData({ showDayPicker: false })
   }
 })
